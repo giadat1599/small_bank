edd15729c64dd37ee16f5aa9eb6b8c79226fcd7a
Gia Dat Truong <giadattruong@Gias-MacBook-Pro.local>
2023-10-01 15:32:53 +0700

    fix: currency code

--- utils/random.go
+++ utils/random.go
@@ -37,7 +37,7 @@ func RandomMoney() int64 {
 
 // RandomCurrency generates a random currency ode
 func RandomCurrency() string {
-	currencies := []string{"EUR", "USD", "VND"}
+	currencies := []string{"EUR", "USD"}
 	n := len(currencies)
 	return currencies[rand.Intn(n)]
 }
