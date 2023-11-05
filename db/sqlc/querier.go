24aba4590f1ba2b848d5e3843d93e595b907362d
Gia Dat Truong <giadattruong@192.168.2.3>
2023-11-05 16:52:08 +0700

    feat: partially update

--- db/sqlc/querier.go
+++ db/sqlc/querier.go
@@ -5,8 +5,7 @@
 package db
 
 import (
-	"context"	
-
+	"context"
 	"github.com/google/uuid"
 )
 
@@ -28,6 +27,7 @@ type Querier interface {
 	ListEntries(ctx context.Context, arg ListEntriesParams) ([]Entry, error)
 	ListTransfers(ctx context.Context, arg ListTransfersParams) ([]Transfer, error)
 	UpdateAccount(ctx context.Context, arg UpdateAccountParams) (Account, error)
+	UpdateUser(ctx context.Context, arg UpdateUserParams) (User, error)
 }
 
 var _ Querier = (*Queries)(nil)
